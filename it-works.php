2a8a699027599f37408baff4158de420035c8975
DominicWieser <dominic.wieser1995@live.at>
2018-04-13 10:19:36 +0200

    adding finished CodeReview-01

---
+++ it-works.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CodeReview-01 | CodeFactory replica | IT WORKS!</title>
+	<!-- Linking to style.css file -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<!-- Container -->
+	<div id="container">
+
+		<!-- Header -->
+		<header>
+			<img class="float-left" src="img/logo.png" alt="Logo-of-CodeFactory">
+			<span>Dominic Wieser - Future Full Stack Web Developer</span>
+		</header>
+		
+		<!-- Navigation -->
+		<nav>
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="aboutme.html">About me</a></li>
+				<li><a href="blog.html">Blog</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</nav>
+
+		<!-- Wrapper -->
+		<div class="wrapper">
+			<h1>YOU SUCCESSFULLY SUBMITTED YOUR FORM ;)</h1>
+		</div>
+	</div>
+</html>
